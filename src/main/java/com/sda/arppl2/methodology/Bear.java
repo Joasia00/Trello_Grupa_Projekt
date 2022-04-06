a0bf1db6b29d5a7689347f3178259bdcebeaa39c
Krystian Kaźmierczak <krystian.kazmierczak91@gmail.com>
2022-04-06 17:25:26 +0200

    stworzona klasa Bear
    stworzone misie

---
+++ src/main/java/com/sda/arppl2/methodology/Bear.java
@@ -0,0 +1,6 @@
+package com.sda.arppl2.methodology;
+
+public class Bear {
+    String imie;
+    int wiek;
+}
--- src/main/java/com/sda/arppl2/methodology/Main.java
+++ src/main/java/com/sda/arppl2/methodology/Main.java
@@ -1,4 +1,25 @@
 package com.sda.arppl2.methodology;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("zadanie 1 zrobione!");
+
+
+        Bear b1 = new Bear();
+        b1.imie = "Marian";
+        b1.wiek = 32;
+
+        Bear b2 = new Bear();
+        b2.imie = "Olek";
+        b2.wiek = 2;
+
+        Bear b3 = new Bear();
+        b3.imie = "Filipek";
+        b3.wiek = 10;
+
+        Bear b4 = new Bear();
+        b4.imie = "Teddy";
+        b4.wiek = 3;
+
+    }
 }
